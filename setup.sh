fa83b86b8ba5cf8ebb65eb718224714bfed5db50
Constantine Tarasenkov <iamvfx@gmail.com>
2022-04-04 01:00:25 +0300

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,7 @@
+mkdir -p /opt/github/procedural/
+cd /opt/github/procedural/
+git clone https://github.com/procedural/redgpu_helper
+git clone https://github.com/procedural/redgpu_memory_allocator_vma
+git clone https://github.com/procedural/redgpu_green_struct
+git clone https://github.com/procedural/redgpu_inline_procedure
+
